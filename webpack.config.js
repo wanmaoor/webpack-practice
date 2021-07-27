43263f1073f511fe64a33d31c2a4750384ad7050
wanmao <wequart@gmail.com>
2021-07-27 08:36:07 +0800

    03: 使用多入口(multiple entry)进行打包

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path')
 module.exports = {
     mode: "production",
-    entry: './src/index.js',
+    entry: {
+        app: path.resolve(__dirname, 'src', 'index.js'),
+        search: path.join(__dirname, '/src', '/search.js')
+    },
     output: {
         path: path.join(__dirname, 'dist'),
-        filename: "bundle.js"
+        filename: "bundle.[name].js"
     }
 }
