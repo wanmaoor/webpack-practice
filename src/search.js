9ce7e0f1b9da235680b56e1724a73e8d87ae5ff0
wanmao <wequart@gmail.com>
2021-07-28 17:13:37 +0800

    06: 文件监听

--- src/search.js
+++ src/search.js
@@ -4,7 +4,8 @@ import './search.less'
 import img from './img/yuhu.jpg'
 class Search extends React.Component{
     render() {
-        return <div className="search-text">Search Text111345651
+        return <div className="search-text">Search Text
+            <img style={{width: '800px'}} src={img} alt=""/>
         </div>
     }
 }
