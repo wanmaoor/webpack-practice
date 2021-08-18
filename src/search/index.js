fbf8241912950a445c716ebaaddb101b1ca70620
wanmao <wequart@gmail.com>
2021-08-18 17:37:16 +0800

    15: 动态import引入

--- src/search/index.js
+++ src/search/index.js
@@ -3,12 +3,28 @@ import ReactDOM from 'react-dom'
 import './search.less'
 import {a} from './treeshaking'
 
+
 class Index extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state= {
+            Text: undefined
+        }
+    }
+
+    loadComponent(){
+        import('./split-code').then(Component => {
+            this.setState({Text: Component.default})
+        })
+    }
     render() {
+        const {Text} = this.state
         const str = a()
         return (<div className="search-text">
             Search Text12311哎哟我
             {str}
+            <button onClick={this.loadComponent.bind(this)}>点击加载组件</button>
+            {Text ? <Text /> : null}
             {/*<img style={{width: '800px'}} src={img} alt=""/>*/}
         </div>)
     }
---
+++ src/search/split-code.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export default () => <div>这是一个分离的组件, 准备动态Import</div>
